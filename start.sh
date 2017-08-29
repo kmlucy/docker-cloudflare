31e521e75949388163e607a399a266614bb7a40e
Jason Gwartz <jason@jasongwartz.com>
2017-08-29 15:45:11 +0200

    Removed default for 'DAEMON_SLEEP_TIME' as it's now in the Dockerfile

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/ash
 
 if [ "$RUN_AS_DAEMON" = 1 ]; then
-    : ${DAEMON_SLEEP_TIME:=600}
     echo "This script will update Cloudflare every $DAEMON_SLEEP_TIME seconds."
     while true; do /cloudflare-update-record.sh; sleep $DAEMON_SLEEP_TIME; done
 else
