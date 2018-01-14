53d8d7372af123ae166ac6c5e7db48afd3a46371
Kyle Lucy <kmlucy@gmail.com>
2018-01-14 10:43:34 -0500

    catch error when no ip retrieved

--- cloudflare-update-record.sh
+++ cloudflare-update-record.sh
@@ -25,9 +25,12 @@ log "Check Initiated"
 
 if [ -f $ip_file ]; then
     old_ip=$(cat $ip_file)
-    if [ $ip == $old_ip ]; then
-        log "IP has not changed."
+    if [ -z "$ip" ]; then
+	log "No IP retrieved"
         exit 0
+    elif [ $ip == $old_ip ]; then
+        log "IP has not changed"
+	exit 0
     fi
 fi
 
